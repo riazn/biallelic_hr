a408ff7c6bb309443ac024d537d155bef097a692
blecuacp <blecuacp@mskcc.org>
2017-05-25 14:57:55 -0400

    Update test_all_figs.R

--- test_all_figs.R
+++ test_all_figs.R
@@ -1,20 +1,20 @@
-print("Test Generate_Fig1a.r")
-source("Generate_Fig1a.r")
+print("Test Generate_Fig1a.R")
+source("Generate_Fig1a.R")
 
-print("Test Generate_Fig1b.r")
-source("Generate_Fig1b.r")
+print("Test Generate_Fig1b.R")
+source("Generate_Fig1b.R")
 
-print("Test Generate_Fig1c.r")
-source("Generate_Fig1c.r")
+print("Test Generate_Fig1c.R")
+source("Generate_Fig1c.R")
 
-print("Test Generate_Fig1d.r")
-source("Generate_Fig1d.r")
+print("Test Generate_Fig1d.R")
+source("Generate_Fig1d.R")
 
-print("Test Generate_Fig1e.r")
-source("Generate_Fig1e.r")
+print("Test Generate_Fig1e.R")
+source("Generate_Fig1e.R")
 
-print("Test Generate_Fig2a.r")
-source("Generate_Fig2a.r")
+print("Test Generate_Fig2a.R")
+source("Generate_Fig2a.R")
 
 print("Test suppl fig 1a")
 source("Generate_Suppl_Fig1a.R")
